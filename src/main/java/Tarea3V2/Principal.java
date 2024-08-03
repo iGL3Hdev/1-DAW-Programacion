5583d42ab5d1939d542e67926bb057cef777ee2e
Your Name <you@example.com>
2024-08-03 21:15:35 +0200

    Finalizado

---
+++ src/main/java/Tarea3V2/Principal.java
@@ -0,0 +1,29 @@
+
+package Tarea3V2;
+
+
+public class Principal {
+    
+    public static void main(String[] args){
+        
+        
+        System.out.println("Primera fecha, inicializada con el primer constructor");
+        Fecha objFecha1 = new Fecha(enumMes.JULIO);
+        
+        objFecha1.setDia(15);
+        objFecha1.setAnio(2021);
+        
+        
+        System.out.println(objFecha1.toString());
+        System.out.println(objFecha1.isSummer() ? "Es verano" : "No es verano");
+    
+    
+        System.out.println("Segunda fecha, inicializada con el segundo constructor");
+        Fecha objFecha2 = new Fecha(15, enumMes.JULIO,2021);
+        
+        System.out.println(objFecha2.getAnio());
+        System.out.println(objFecha2.toString());
+        System.out.println(objFecha2.isSummer() ? "Es verano" : "No es verano");
+
+    }
+}
